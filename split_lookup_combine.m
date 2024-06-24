9b63d1f5e7c137ec5c5bab55158d32036c0a3e8f
Duncan King <duncan.king@company-net.com>
2024-06-24 16:19:18 +0100

    Adds M query file for custom function

---
+++ split_lookup_combine.m
@@ -0,0 +1,15 @@
+let
+    Source = (ListContent as any) => let
+        Source = ListContent,
+        #"Split Text" = Text.Split(Source, "|"),
+        #"Converted to Table" = Table.FromList(#"Split Text", Splitter.SplitByNothing(), null, null, ExtraValues.Error),
+        #"Changed Type" = Table.TransformColumnTypes(#"Converted to Table",{{"Column1", Int64.Type}}),
+        #"Merged Queries" = Table.NestedJoin(#"Changed Type", {"Column1"}, Lookup, {"ID"}, "Lookup", JoinKind.LeftOuter),
+        #"Expanded Lookup" = Table.ExpandTableColumn(#"Merged Queries", "Lookup", {"Value"}, {"Lookup.Value"}),
+        #"Removed Columns" = Table.RemoveColumns(#"Expanded Lookup",{"Column1"}),
+        #"Converted to List" = Table.ToList(#"Removed Columns"),
+        #"Combined Text" = Text.Combine(#"Converted to List", "|")
+    in
+        #"Combined Text"
+in
+    Source
